1f14c614928e82f6b23e70c025b4eac1b2e5eab0
Uthpalawanna <uthpalawanna30@gmail.com>
2026-02-17 23:35:28 +0530

    E - Learn

--- elearn-app/src/App.js
+++ elearn-app/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import "./App.css"
+import Header from "./components/common/heading/Header"
+import { BrowserRouter as Router, Switch } from "react-router-dom"
+
 
 function App() {
   return (
     <>
+    <Router>
       <Header />
+      <Switch>
+
+      </Switch>
+      </Router>
     </>
   )
 }
--- elearn-app/src/components/common/heading/Header.jsx
+++ elearn-app/src/components/common/heading/Header.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import Head from "./Head"
+import Head from "./Head";
+import "./header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <div>
+        <>
             <Head />
-        </div>
-    )
-}
+            <header>
+                <nav className="flexSB">
+                    <ul className="flexSB">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/courses">All Courses</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/team">Team</Link></li>
+                        <li><Link to="/pricing">Pricing</Link></li>
+                        <li><Link to="/journal">Journal</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                    </ul>
+                    <div className="start">
+                        <div className="button">GET CERTIFICATE</div>
+                    </div>
+                        
+                </nav>
+            </header>
+        </>
+    );
+};
 
-export default Header
+export default Header;
