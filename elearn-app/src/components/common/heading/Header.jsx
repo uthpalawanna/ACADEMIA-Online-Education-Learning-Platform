2ca248555577565ea5171a54d6529f8fddbd46db
Uthpalawanna <uthpalawanna30@gmail.com>
2026-02-18 14:02:15 +0530

    E - Learn

--- elearn-app/src/components/common/heading/Header.jsx
+++ elearn-app/src/components/common/heading/Header.jsx
@@ -1,31 +1,49 @@
-import React from "react";
-import Head from "./Head";
-import "./header.css";
-import { Link } from "react-router-dom";
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import Head from "./Head"
+import "./header.css"
 
 const Header = () => {
-    return (
-        <>
-            <Head />
-            <header>
-                <nav className="flexSB">
-                    <ul className="flexSB">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/courses">All Courses</Link></li>
-                        <li><Link to="/about">About</Link></li>
-                        <li><Link to="/team">Team</Link></li>
-                        <li><Link to="/pricing">Pricing</Link></li>
-                        <li><Link to="/journal">Journal</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
-                    </ul>
-                    <div className="start">
-                        <div className="button">GET CERTIFICATE</div>
-                    </div>
-                        
-                </nav>
-            </header>
-        </>
-    );
-};
+  const [click, setClick] = useState(false)
 
-export default Header;
+  return (
+    <>
+      <Head />
+      <header>
+        <nav className='flexSB'>
+          <ul className={click ? "mobile-nav" : "flexSB "} onClick={() => setClick(false)}>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/courses'>All Courses</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/team'>Team</Link>
+            </li>
+            <li>
+              <Link to='/pricing'>Pricing</Link>
+            </li>
+            <li>
+              <Link to='/journal'>Journal</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+          </ul>
+          <div className='start'>
+            <div className='button'>GET CERTIFICATE</div>
+          </div>
+          <button className='toggle' onClick={() => setClick(!click)}>
+            {click ? <i className='fa fa-times'> </i> : <i className='fa fa-bars'></i>}
+          </button>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default Header
