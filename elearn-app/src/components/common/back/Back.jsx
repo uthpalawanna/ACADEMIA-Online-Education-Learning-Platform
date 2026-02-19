4519c6ffbd9d496f4eda01d1025aea9ac90b3f76
Uthpalawanna <uthpalawanna30@gmail.com>
2026-02-19 21:09:44 +0530

    E - Learn

--- elearn-app/src/components/common/back/Back.jsx
+++ elearn-app/src/components/common/back/Back.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
-import title from '../title/title'
 
-const Back = () => {
+const Back = ({title}) => {
     const location = useLocation()
   return (
     <div>
         <section className='back'>
-            <h2>Home /{location.pathname.split("/")}</h2>
+            <h2>Home  / {location.pathname.split("/")[1]}</h2>
             <h1>{title}</h1>
         </section>
     </div>
