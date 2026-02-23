6ae4929c0855dd18da09ace2985e129697fab701
Uthpalawanna <uthpalawanna30@gmail.com>
2026-02-23 21:57:19 +0530

    E - Learn

--- elearn-app/src/App.js
+++ elearn-app/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch,Route } from "react-router-dom"
 import Home from "./components/home/Home"
 import About from "./components/about/About"
 import CourseHome from "./components/allcourses/CourseHome"
+import Team from "./components/team/Team"
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Route path='/' exact component={Home} />
         <Route path='/about' exact component={About} />
         <Route path='/courses' exact component={CourseHome} />
+        <Route path='/team' exact component={Team} />
+
       </Switch>
       </Router>
     </>
